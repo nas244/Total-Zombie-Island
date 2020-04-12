86d2100477dd523134880edf6c4405b736da16dd
M4Rs-dev <57373979+M4Rs-dev@users.noreply.github.com>
2020-04-11 20:54:43 -0500

    Add files via upload

---
+++ Total Zombie Island/Assets/Scripts/EquipingWeapon.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class EquipingWeapon : MonoBehaviour
+{
+    public GameObject Shotgun;
+    public GameObject SMG;
+    public GameObject Hand;
+
+    public void HandWeapon ()
+    {
+        Shotgun.gameObject.SetActive(false);
+        SMG.gameObject.SetActive(false);
+        Hand.gameObject.SetActive(true);
+    }
+    public void ShotgunWeapon ()
+    {
+        SMG.gameObject.SetActive(false);
+        Shotgun.gameObject.SetActive(true);
+    }
+    public void SMGWeapon ()
+    {
+        Hand.gameObject.SetActive(false);
+        Shotgun.gameObject.SetActive(false);
+        SMG.gameObject.SetActive(true);
+    }
+}
